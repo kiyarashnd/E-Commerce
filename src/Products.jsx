c9ad4415c70a526dc22154186b3c4fb35ffefdb8
Kiyarash <kiarashnadri@yahoo.com>
2023-07-04 21:48:41 +0330

    change style of filters and work on search box

--- src/Products.jsx
+++ src/Products.jsx
@@ -5,54 +5,89 @@ import myStyles from './Products.module.css';
 import { useState } from 'react';
 
 const Products = () => {
-  console.log(products);
+  let data = products;
   const [val, setVal] = useState(10);
+
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+  data = data.filter((product) =>
+    product.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  function handelSubmit(eve) {
+    eve.preventDefault();
+  }
   return (
     <>
       <BreadCrumbs />
+      <p>{`${data.length} item found`}</p>
       <section className={myStyles.container}>
-        <aside className={myStyles.sidebar}>
-          <input type='text' placeholder='search' />
-          <h5>Category</h5>
-          <button>All</button>
-          <button>Office</button>
-          <button>Living Room</button>
-          <button>Kitchen</button>
-          <button>Bedroom</button>
-          <button>Dining</button>
-          <button>Kids</button>
+        <form onSubmit={handelSubmit}>
+          <aside className={myStyles.sidebar}>
+            <div>
+              <input
+                type='text'
+                placeholder='search'
+                value={searchTerm}
+                onChange={handleChange}
+                className={myStyles.inputText}
+              />
+            </div>
+            <div className={myStyles['form-control']}>
+              <h5 className={myStyles.head5}>Category</h5>
+              <div>
+                <button className={myStyles.btn}>All</button>
+                <button className={myStyles.btn}>Office</button>
+                <button className={myStyles.btn}>Living Room</button>
+                <button className={myStyles.btn}>Kitchen</button>
+                <button className={myStyles.btn}>Bedroom</button>
+                <button className={myStyles.btn}>Dining</button>
+                <button className={myStyles.btn}>Kids</button>
+              </div>
+            </div>
+            <div className={myStyles['form-control']}>
+              <h5 className={myStyles.head5}>Company</h5>
+              <select name='comp' id='company' className={myStyles.comp}>
+                <option value='all'>all</option>
+                <option value='marcos'>marcos</option>
+                <option value='liddy'>liddy</option>
+                <option value='ikea'>ikea</option>
+                <option value='caressa'>caressa</option>
+              </select>
+            </div>
 
-          <label htmlFor='company'>Company</label>
-          <select name='cars' id='company'>
-            <option value='all'>all</option>
-            <option value='marcos'>marcos</option>
-            <option value='liddy'>liddy</option>
-            <option value='ikea'>ikea</option>
-            <option value='caressa'>caressa</option>
-          </select>
+            <div className={myStyles['form-control']}>
+              <h5 className={myStyles.head5}>price</h5>
 
-          {/* <input
+              {/* <input
             type='range'
             value='24'
             min='1'
             max='100'
             oninput='this.nextElementSibling.value = this.value'
           /> */}
-          <input
-            // value={val}
-            type='range'
-            min='0'
-            max='9999999999'
-            step='1'
-            // onchange={setVal(this.value)}
-          ></input>
-          <output>{val}</output>
-          <button>clear filters</button>
-        </aside>
+              <input
+                // value={val}
+                type='range'
+                min='0'
+                max='9999999999'
+                step='1'
+                // onchange={setVal(this.value)}
+              ></input>
+              <output>{val}</output>
+            </div>
+            <div className={myStyles['form-control']}>
+              <button className={myStyles.clearBtn}>clear filters</button>
+            </div>
+          </aside>
+        </form>
         <div>
           <header>header</header>
           <main className={myStyles.productsContainer}>
-            {products.map((item) => {
+            {data.map((item) => {
               return (
                 <Link
                   to={`/Products/${item.id}`}
